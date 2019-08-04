068e0f280dec84993b0aaa039113bab548cdc75e
anirudh duggal <anirudhdggl@gmail.com>
2019-08-04 22:13:46 +0530

    Create centuar.cpp

---
+++ centuar.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    system("cls");
+    cout<<"\t\t  <";
+    cout<<"=======]}======"<<endl;
+    cout<<"\t\t    --.   /|"<<endl;
+    cout<<"\t\t   _\\\"/_.'/"<<endl;
+    cout<<"\t\t .'._._,.'"<<endl;
+    cout<<"\t\t :/ \\{}/"<<endl;
+    cout<<"\t\t(L  /--',----._"<<endl;
+    cout<<"\t\t    |          \\\\"<<endl;
+    cout<<"\t\t   : /-\\ .'-'\\ / |"<<endl;
+    cout<<"\t\t    \\\\, ||    \\|"<<endl;
+    cout<<"\t\t     \\/ ||    ||"<<endl;
+    return 0;
+}
